8f8fe277c9dc3761895ecc5e4f5b06dce83b1736
VCEQZL <167464208+VCEQZL@users.noreply.github.com>
2025-01-16 10:54:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -106,4 +106,5 @@ def index():
     return render_template("index.html", url=None, result=None)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5600)))
+    port = int(os.environ.get("PORT", 5400))  # Use the PORT environment variable, or default to 5000
+    app.run(host="0.0.0.0", port=port)
